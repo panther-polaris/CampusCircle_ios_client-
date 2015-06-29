000c0fcd2ccfa71bb863fbb82b3418fa03ffddf6
guangchengli <sky9475@126.com>
2015-06-29 00:40:16 +0000

    Initial Commit

---
+++ CampusCircle_ios_clientTests/CampusCircle_ios_clientTests.swift
@@ -0,0 +1,36 @@
+//
+//  CampusCircle_ios_clientTests.swift
+//  CampusCircle_ios_clientTests
+//
+//  Created by jwq on 15/6/29.
+//  Copyright (c) 2015年 panther-polaris. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CampusCircle_ios_clientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
